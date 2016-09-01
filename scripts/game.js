4b0b7a715304756fb33350f600292b613f1276b8
Brent Reno <brent.reno@gmail.com>
2016-09-01 11:02:46 -0500

    Two player mode is working with a test div

--- scripts/game.js
+++ scripts/game.js
@@ -1,26 +1,30 @@
 //Initiate handshake
 console.log("Games.js is sourced");
-var userInput;
+var playerOneInput;
+var playerTwoInput;
 var games=0;
 var computerChoice;
 var rocksChosen = 0;
 var papersChosen = 0;
 var scissorsChosen =  0;
+var hideSinglePlayer = function(){
+  $("#singlePlayer").hide();
+}; //hide singlePlayer
 
-// Decide one or two player game
-//if one player game
-
-
- var playGame = function(){
+//Run single player game
+ var playGameSolo = function(){
   console.log("Inside PlayGame");
-
+//Show Single Player game
 // get user input
-userInput = document.getElementById('userInput').value;
-console.log(userInput);
+playerOneInput = document.getElementById('playerOneIn').value;
+console.log(playerOneInput);
 // create computer choice
 computerInput();
-compareInputs(userInput, computerInput);
+compareInputs(playerOneInput, computerInput);
 };//end play game
+
+//run two player game
+var playGameTwo = function(){};
 
   //compare user and computer inputs
   var compareInputs = function(userInput, computerInput){
@@ -75,13 +79,24 @@ var computerInput =function(){
 
 //display games played on DOM
 $(document).ready(function(){
-  console.log("Document is ready");
+console.log("Document is ready");
+//Single Player onClick
+ $('#soloPlay').on("click", function(){
+   console.log('in soloPlay OnClick');
+   $('#singlePlayer').show();
+ });
+
+ //run single player mode
   $("#gamesPlayed").on('click', function(){
     games++;
     console.log(games);
     $("#outcomesDiv").html("<p id='totalGames'> You have played " + games +" game(s).</p>");
     // store user choices functionand append to div
-    choicesTally(userInput);
+    choicesTally(playerOneInput);
+  });
+  $("#twoPlay").on('click', function(){
+    console.log('In twoPlay onClick');
+    $("#playerSelection").append('<div id="twoPlay">TEST</div>');
   });
 });
 
